970ed3c47b421fac1c73e67e0026114c5a773d3c
raghavsevak <raghavsevak744@gmail.com>
2025-01-26 00:46:00 +0530

    stageOfLife Completed

--- ProgrammingFundamentals/Ep2/StageOfLife/src/Task.kt
+++ ProgrammingFundamentals/Ep2/StageOfLife/src/Task.kt
@@ -2,11 +2,11 @@ import com.faangx.ktp.basics.StageOfLifeMiniApp
 
 fun stageOfLife(age: Int): String {
     return when {
-        age >= 60 -> "old age"
-        age >= 18 -> "adulthood"
-        age >= 12 -> "teenage"
-        age >= 3 -> "childhood"
-        else -> "infancy"
+        age >= 60 -> "Old age"
+        age >= 18 -> "Adulthood"
+        age >= 12 -> "Teenage"
+        age >= 3 -> "Childhood"
+        else -> "Infancy"
     }
 }
 
