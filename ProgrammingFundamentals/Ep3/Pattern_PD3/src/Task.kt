4b88ce2f8121bef6dd5cd5d920638897bf7926f4
raghavsevak <raghavsevak744@gmail.com>
2025-01-31 18:12:30 +0530

    Pattern PD3 Completed

--- ProgrammingFundamentals/Ep3/Pattern_PD3/src/Task.kt
+++ ProgrammingFundamentals/Ep3/Pattern_PD3/src/Task.kt
@@ -1,7 +1,20 @@
 import com.faangx.ktp.patterns.single.PatternLinesAndTwoCharsBasedMiniApp
 
 fun printPattern(lines: Int, char1: Char, char2: Char) {
-    TODO()
+    repeat(lines) {i ->
+        // space
+        repeat(i) { print(' ') }
+
+        //chars
+        repeat(2 * (lines - i) - 1) { j ->
+            print(
+                if(j%2 == 0) char1 else char2
+            )
+        }
+
+        // break the line
+        println()
+    }
 }
 
 fun main() {
