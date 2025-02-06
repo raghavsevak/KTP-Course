c78d2c6682350d8c4e65e86bd4fcf2d7af80fdb4
raghavsevak <raghavsevak744@gmail.com>
2025-02-06 12:26:59 +0530

    Pattern 3 ep3.ext Completed

--- ProgrammingFundamentals/Ep3.Ext/Pattern3/src/Task.kt
+++ ProgrammingFundamentals/Ep3.Ext/Pattern3/src/Task.kt
@@ -1,9 +1,16 @@
 import com.faangx.ktp.patterns.single.PatternLinesAndTwoCharsBasedMiniApp
 
 fun printPattern(lines: Int, char1: Char, char2: Char) {
-    TODO()
+    repeat(lines) { i ->
+        repeat(2 * lines - 1) { j ->
+            if (j == lines - i - 1 || j == lines + i - 1)
+                print(char1)
+            else
+                print(char2)
+        }
+        println()
+    }
 }
-
 fun main() {
     PatternLinesAndTwoCharsBasedMiniApp("P3", ::printPattern)
 }
