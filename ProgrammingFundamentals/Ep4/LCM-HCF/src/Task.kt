cb8907dd5bcecb145effe46110fe05554fa2279e
raghavsevak <raghavsevak744@gmail.com>
2025-02-08 00:34:12 +0530

    LCM-HCF Completed

--- ProgrammingFundamentals/Ep4/LCM-HCF/src/Task.kt
+++ ProgrammingFundamentals/Ep4/LCM-HCF/src/Task.kt
@@ -1,11 +1,26 @@
 import com.faangx.ktp.basics.LcmHcfCalculatorMiniApp
 
 fun getLCM(x: Int, y: Int): Int {
-    TODO()
+    val a = if (x > y) x else y
+    val b = if (x < y) x else y
+
+    var mul = a
+    while (mul % b != 0) {
+            mul += a
+    }
+    return mul
 }
 
 fun getHCF(x: Int, y: Int): Int {
-    TODO()
+    var a = x
+    var b = y
+
+    while (b != 0) {
+        val rem = a%b
+        a = b
+        b = rem
+    }
+    return a
 }
 
 fun main() {
