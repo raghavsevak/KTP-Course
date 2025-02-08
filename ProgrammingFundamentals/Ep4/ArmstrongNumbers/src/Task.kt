3a8540517d546232f825077841af136ed0697cca
raghavsevak <raghavsevak744@gmail.com>
2025-02-08 12:02:23 +0530

    ArmstrongNumbers Completed

--- ProgrammingFundamentals/Ep4/ArmstrongNumbers/src/Task.kt
+++ ProgrammingFundamentals/Ep4/ArmstrongNumbers/src/Task.kt
@@ -1,19 +1,40 @@
 import com.faangx.ktp.basics.ArmstrongNumbersMiniApp
 
 fun printArmstrongNums(upTo: Int) {
-    TODO()
+    for (i in 0..upTo) {
+        if (isArmstrong(i)) {
+                print("$i, ")
+        }
+    }
 }
 
 fun isArmstrong(num: Int): Boolean {
-    TODO()
+    val n = noOfDigits(num)
+
+    var sum = 0
+    var x = num
+    while (x > 0) {
+        val digit = x % 10
+        x /= 10
+        sum += pow(digit, n)
+    }
+    return sum == num
 }
 
 fun noOfDigits(num: Int): Int {
-    TODO()
+    var x = num
+    var digits = 0
+    while (x > 0) {
+        x /= 10
+        digits += 1
+    }
+    return digits
 }
 
 fun pow(base: Int, exp: Int): Int {
-    TODO()
+    var res = 1
+    repeat(exp) { res *= base }
+    return res
 }
 
 fun main() {
