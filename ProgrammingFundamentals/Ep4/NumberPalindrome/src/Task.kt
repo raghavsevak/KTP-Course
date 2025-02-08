ed3f4868a9465e79fe0a7fdc7e922262a2475e65
raghavsevak <raghavsevak744@gmail.com>
2025-02-08 11:51:19 +0530

    NumberPalindrome Completed

--- ProgrammingFundamentals/Ep4/NumberPalindrome/src/Task.kt
+++ ProgrammingFundamentals/Ep4/NumberPalindrome/src/Task.kt
@@ -1,11 +1,19 @@
 import com.faangx.ktp.basics.NumberPalindromeCheckerMiniApp
 
 fun reverseNum(num: Long): Long {
-    TODO()
+    var reverse = 0L
+    var x = num
+
+    while (x>0) {
+        val rem = x % 10
+        x /= 10
+        reverse = reverse * 10 + rem
+    }
+    return reverse
 }
 
-fun isPalindrome(num: Long): Boolean {
-    TODO()
+fun isPalindrome(num: Long): Boolean  {
+    return reverseNum(num) == num
 }
 
 fun main() {
