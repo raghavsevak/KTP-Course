8486f247778984e94d80243d4e4127734858408c
raghavsevak <raghavsevak744@gmail.com>
2025-02-18 00:06:34 +0530

    StringCaseConvertor Completed

--- ProgrammingFundamentals/Ep4/StringCaseConvertor/src/Task.kt
+++ ProgrammingFundamentals/Ep4/StringCaseConvertor/src/Task.kt
@@ -1,32 +1,97 @@
 import com.faangx.ktp.basics.StringCaseConvertorMiniApp
 
 fun convertToSentenceCase(text: String): String {
-    TODO()
+    var capitalize = true
+    var requireSpace = false
+    return buildString {
+        for (char in text) {
+            if (requireSpace) {
+                if (char != ' ') append(' ')
+                requireSpace = false
+            }
+            if (isAlphabet(char) && capitalize) {
+                append(convertToUppercase(char))
+                capitalize = false
+            } else {
+                append(char)
+            }
+            if (char in listOf('.', '!', '?')) {
+                capitalize = true
+                requireSpace = true
+            }
+        }
+    }
 }
 
 fun convertToTitleCase(text: String): String {
-    return "TODO()"
+    var capitalize = true
+    return buildString {
+        for (char in text) {
+            if (char != ' ') {
+                if (capitalize) {
+                    append(convertToUppercase(char))
+                    capitalize = false
+                } else {
+                    append(char)
+                }
+            }
+            if (char in listOf(' ', '!', '?', '.', ":", ';', ',')) {
+                capitalize = true
+            }
+        }
+    }
 }
 
 fun convertToUppercase(text: String): String {
-    return "TODO()"
+    return buildString {
+        for (char in text) {
+            append(convertToUppercase(char))
+        }
+    }
 }
 
 fun convertToLowercase(text: String): String {
-    return "TODO()"
+    return buildString {
+        for (char in text) {
+            append(convertToLowercase(char))
+        }
+    }
 }
 
 fun convertToSnakeCase(text: String): String {
-    return "TODO()"
+    return buildString {
+        for (char in text) {
+            if (char == ' ') {
+                append('_')
+            } else {
+                append(char)
+            }
+        }
+    }
 }
 
 fun convertToUppercase(char: Char): Char {
-    TODO()
+    return if (char in 'a'..'z') {
+        val upperChar = char - 32
+        (upperChar)
+    } else {
+        char
+    }
 }
 
 fun convertToLowercase(char: Char): Char {
-    TODO()
+    return if (char in 'A'..'Z') {
+        val lowerChar = char + 32
+        (lowerChar)
+    } else {
+        char
+    }
 }
+
+fun isAlphabet(char: Char): Boolean {
+    return char in 'a'..'z' || char in 'A'..'Z'
+}
+
 
 fun main() {
     StringCaseConvertorMiniApp(
