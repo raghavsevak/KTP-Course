f827ac5791f641d9fc2e1b16d49c38c4779c8135
raghavsevak <raghavsevak744@gmail.com>
2025-02-17 23:40:34 +0530

    StringPalindrome Completed

--- ProgrammingFundamentals/Ep4/StringPalindrome/src/Task.kt
+++ ProgrammingFundamentals/Ep4/StringPalindrome/src/Task.kt
@@ -1,24 +1,39 @@
 import com.faangx.ktp.basics.StringPalindromeCheckerMiniApp
 
 fun reverseStr(str: String): String {
-    TODO()
+    return buildString { 
+        for (i in str.lastIndex downTo 0) {
+            append(str[i])
+        }
+    }
 }
 
 fun isPalindrome(str: String, ignoreCase: Boolean): Boolean {
-    TODO()
+    return areStringsEqual(str, reverseStr(str), ignoreCase)
 }
 
 fun areStringsEqual(s1: String, s2: String, ignoreCase: Boolean): Boolean {
-    TODO()
-}
+    if (s1.length != s2.length) return false
+    if (s1.length == 0 && s2.length == 0) return false
 
+    for (i in s1.indices) {
+        if (!areCharsEqual(s1[i], s2[i], ignoreCase)) return false
+    }
+    return true
+}    
 fun areCharsEqual(c1: Char, c2: Char, ignoreCase: Boolean): Boolean {
-    TODO()
+    if (!ignoreCase) return c1 == c2
+    return convertToLowercase(c1) == convertToLowercase(c2)
 }
 
 fun convertToLowercase(char: Char): Char {
-    TODO()
+    return if (char in 'A'..'Z') {
+            Char(char.code + 32)
+    } else {
+            char
+    }
 }
+
 
 fun main() {
     StringPalindromeCheckerMiniApp(
