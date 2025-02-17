e25366d24f8ff57e68a598860d6626e24596174b
raghavsevak <raghavsevak744@gmail.com>
2025-02-17 23:59:56 +0530

    StringStatsCalculator Completed

--- ProgrammingFundamentals/Ep4/StringStatsCalculator/src/Task.kt
+++ ProgrammingFundamentals/Ep4/StringStatsCalculator/src/Task.kt
@@ -1,39 +1,78 @@
 import com.faangx.ktp.basics.StringStatsCalculatorMiniApp
 
 fun calcNoOfChars(text: String): Int {
-    TODO()
+    return text.length
 }
 
 fun calcNoOfAlphabets(text: String): Int {
-    return 0
+    var count = 0
+    for (c in text) {
+        if (c in 'a'..'z' || c in 'A'..'Z') count += 1
+    }
+    return count
 }
 
 fun calcNoOfVowels(text: String): Int {
-    return 0
+    var count = 0
+    for (c in text) {
+        if (c in listOf('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U')) count += 1
+    }
+    return count
 }
 
 fun calcNoOfConsonants(text: String): Int {
-    return 0
+    return calcNoOfAlphabets(text) - calcNoOfVowels(text)
 }
 
 fun calcNoOfDigits(text: String): Int {
-    return 0
+    var count = 0
+    for (c in text) {
+        if (c in '0'..'9') count += 1
+    }
+    return count
 }
 
 fun calcNoOfSpecialChars(text: String): Int {
-    return 0
+    return text.length - (calcNoOfAlphabets(text) + calcNoOfDigits(text) + calcNoOfSpaces(text))
 }
 
 fun calcNoOfSpaces(text: String): Int {
-    return 0
+    var count = 0
+    for (c in text) {
+        if (c in listOf(' ', '\n', '\t')) count += 1
+    }
+    return count
 }
 
 fun calcNoOfWords(text: String): Int {
-    return 0
+    var count = 0
+    var nonPunctuationCharFound = false
+    for (c in text) {
+        if (c !in listOf(' ', '\t', '\n', '.', ',', '?', '!')) {
+            nonPunctuationCharFound = true
+        } else if (nonPunctuationCharFound) {
+            nonPunctuationCharFound = false
+            count += 1
+        }
+    }
+    if (nonPunctuationCharFound) count += 1
+    return count
 }
 
 fun calcNoOfSentences(text: String): Int {
-    return 0
+    var count = 0
+    var nonPunctuationCharFound = false
+
+    for (c in text) {
+        if (c !in listOf(' ', '\t', '\n', '.', ',', '?', '!')) {
+            nonPunctuationCharFound = true
+        } else if (c in listOf('.', '!', '?') && nonPunctuationCharFound) {
+            nonPunctuationCharFound = false
+            count += 1
+        }
+    }
+    if (nonPunctuationCharFound) count += 1
+    return count
 }
 
 fun main() {
