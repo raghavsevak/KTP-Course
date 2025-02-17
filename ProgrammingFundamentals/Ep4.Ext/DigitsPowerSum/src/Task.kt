426579f79a65cd9fc47d514dd93c7209a8bb4ce4
raghavsevak <raghavsevak744@gmail.com>
2025-02-18 00:18:50 +0530

    DigitsPowerSum Completed

--- ProgrammingFundamentals/Ep4.Ext/DigitsPowerSum/src/Task.kt
+++ ProgrammingFundamentals/Ep4.Ext/DigitsPowerSum/src/Task.kt
@@ -1,5 +1,18 @@
 fun getDigitsPowerSum(num: Int): Int {
-    TODO()
+    var result = 0
+    var x = num
+    var power = 1
+    while (x > 0){
+        var digit = x % 10
+        var n = 1
+        repeat(power){
+            n *= digit
+        }
+        result += n
+        x /= 10
+        power++
+    }
+    return result
 }
 
 fun main() {
