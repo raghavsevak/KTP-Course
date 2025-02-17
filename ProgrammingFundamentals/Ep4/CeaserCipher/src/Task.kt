f2058f6ce4801673f541d6b0b15d92fdd23ad0a5
raghavsevak <raghavsevak744@gmail.com>
2025-02-17 23:52:51 +0530

    CeaserCipher Completed

--- ProgrammingFundamentals/Ep4/CeaserCipher/src/Task.kt
+++ ProgrammingFundamentals/Ep4/CeaserCipher/src/Task.kt
@@ -1,27 +1,39 @@
 import com.faangx.ktp.basics.CeaserCipherMiniApp
 
 fun encode(text: String, delta: Int): String {
-    TODO()
+    return buildString {
+        for (char in text) {
+            if (isLetter(char)) {
+                append(shiftChar(char, delta))
+            } else {
+                append(char)
+            }
+        }
+    }
 }
 
 fun decode(text: String, delta: Int): String {
-    TODO()
+    return encode(text, -delta)
 }
 
 fun encode(text: String, delta: Int, negative: Boolean): String {
-    TODO()
+    return encode(text, if (negative) -delta else delta)
 }
 
 fun decode(text: String, delta: Int, negative: Boolean): String {
-    TODO()
+    return encode(text, if (negative) delta else -delta)
 }
 
 fun shiftChar(char: Char, delta: Int): Char {
-    TODO()
+    val start = if (char in 'A'..'Z') 'A' else 'a'
+    val position = char.code - start.code
+    var shifted = (position + delta) % 26
+    if (shifted < 0) shifted += 26
+    return Char(start.code + shifted)
 }
 
 fun isLetter(char: Char): Boolean {
-    TODO()
+    return char in 'a'..'z' || char in 'A'..'Z'
 }
 
 fun main() {
